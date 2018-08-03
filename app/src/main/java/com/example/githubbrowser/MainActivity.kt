4e0eb51128c26d0ff0cdd44aceaaed1bb72aec96
Patrick <patrick85081@gmail.com>
2018-08-04 06:32:19 +0800

    Databinding 設定

--- app/src/main/java/com/example/githubbrowser/MainActivity.kt
+++ app/src/main/java/com/example/githubbrowser/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.githubbrowser
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
+import com.example.githubbrowser.databinding.MainActivityBinding
 
 /**
  * Created by Patrick on 2018/8/4.
  */
 class MainActivity : AppCompatActivity()
 {
-    private val btnRefresh: Button by lazy { findViewById<Button>(R.id.btnRefresh) }
+    private lateinit var binding: MainActivityBinding;
+
     private val viewModel: MainViewModel = MainViewModel();
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.main_activity);
-
-        btnRefresh.setOnClickListener {
+        //setContentView(R.layout.main_activity);
+        binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity);
+        binding.btnRefresh.setOnClickListener {
             viewModel.refresh();
         }
     }
